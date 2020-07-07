d637f694f1a7c620044c93ef11bf355accfe2e8a
lazzyq <zengqiang96@gmail.com>
2020-07-07 19:07:12 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/wmsx/menger_api
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/micro/go-micro/v2 v2.9.1
+)
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/micro/go-micro/v2/util/log"
+	"github.com/micro/go-micro/v2/web"
+)
+
+const name = "wm.sx.web.menger"
+
+func main() {
+	svc := web.NewService(
+		web.Name(name),
+	)
+
+	if err := svc.Init(); err != nil {
+		log.Fatal("初始化失败", err)
+	}
+
+	router := gin.Default()
+	//mengerHandler := handler.New(svc.Options().Service.Client())
+	r := router.Group("/menger")
+	r.GET("/login", mengerHandler.Login)
+
+	svc.Handle("/", router)
+
+	if err := svc.Run(); err != nil {
+		log.Fatal("启动失败", err)
+	}
+}
