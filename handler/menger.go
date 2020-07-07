91e66de781f8d6bf08693c0089e91b33eeb0a32f
lazzyq <zengqiang96@gmail.com>
2020-07-07 22:24:56 +0800

    测试

--- handler/menger.go
+++ handler/menger.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/micro/go-micro/v2/client"
+	"github.com/micro/go-micro/v2/util/log"
 	menger "github.com/wmsx/menger_svc/proto/menger"
 	"net/http"
 )
@@ -20,21 +22,20 @@ func New(client client.Client) *MengerApiService {
 }
 
 func (m *MengerApiService) Login(c *gin.Context) {
-	//var (
-	//	loginRes *menger.LoginResponse
-	//	err      error
-	//)
+	var (
+		loginRes *menger.LoginResponse
+		err      error
+	)
 
-	//if loginRes, err = m.mengerClient.Login(context.Background(), &menger.LoginRequest{}); err != nil {
-	//	log.Fatal("调用wm.sx.svc.menger失败", err)
-	//	c.JSON(http.StatusInternalServerError, map[string]string{
-	//		"message": "调用wm.sx.svc.menger失败",
-	//	})
-	//	return
-	//}
+	if loginRes, err = m.mengerClient.Login(context.Background(), &menger.LoginRequest{}); err != nil {
+		log.Fatal("调用wm.sx.svc.menger失败", err)
+		c.JSON(http.StatusInternalServerError, map[string]string{
+			"message": "调用wm.sx.svc.menger失败",
+		})
+		return
+	}
 
 	c.JSON(http.StatusOK, map[string]string{
-		//"message": loginRes.Msg,
-		"message": "sucess",
+		"message": loginRes.Msg,
 	})
 }
